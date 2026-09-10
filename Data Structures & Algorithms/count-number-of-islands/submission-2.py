e466d65a8dc6bd9e3e3f9d89160019d77b18a73a
GOKUL M K <gokulmuthuisme@gmail.com>
2026-09-10 08:59:09 +0530

    Add: count-number-of-islands - submission-2

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-2.py
@@ -0,0 +1,25 @@
+class Solution:
+    def dfs(self, r: int, c: int, grid: List[List[str]], rows: int, cols: int, seen: set()):
+        q=collections.deque()
+        seen.add((r, c))
+        q.append((r, c))
+        while q:
+            row, col = q.pop()
+            dirs=[[1, 0], [-1, 0], [0, 1], [0, -1]]
+            for dr, dc in dirs:
+                r=row+dr
+                c=col+dc
+                if (r in range(rows) and
+                c in range(cols) and grid[r][c]=="1" and (r, c) not in seen):
+                    seen.add((r, c))
+                    q.append((r, c))
+    def numIslands(self, grid: List[List[str]]) -> int:
+        rows, cols = len(grid), len(grid[0])
+        islands=0
+        seen=set()
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c]=="1" and (r, c) not in seen:
+                    self.dfs(r, c, grid, rows, cols, seen)
+                    islands+=1
+        return islands
